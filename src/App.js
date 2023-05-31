daa6ecede2368bb699b53a8e734602dc706320be
parthSata <parthsata1709@gmail.com>
2023-05-31 19:04:23 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import NavBar from './Components/Navbar';
 import News from './Components/News';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 export default class App extends Component {
@@ -9,11 +9,11 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <Router>
+          <Routes>
         <NavBar />
         <News pageSize={5} country="in" category="science" />
-        <Router>
           <NavBar />
-          <Switch>
             <Route exact path="/"><News key="general" pageSize={5} country="in" category="general" /></Route>
             <Route exact path="/business"><News key="business" pageSize={5} country="in" category="business" /></Route>
             <Route exact path="/entertainment"><News key="entertainment" pageSize={5} country="in" category="entertainment" /></Route>
@@ -22,7 +22,7 @@ export default class App extends Component {
             <Route exact path="/science"><News key="science" pageSize={5} country="in" category="science" /></Route>
             <Route exact path="/sports"><News key="sports" pageSize={5} country="in" category="sports" /></Route>
             <Route exact path="/technology"><News key="technology" pageSize={5} country="in" category="technology" /></Route>
-          </Switch>
+          </Routes>
         </Router>
       </div>
     )
