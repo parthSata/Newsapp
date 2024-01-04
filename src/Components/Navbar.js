385ac2d5d21fa60355ec9e19e3f364376812db7f
Hemal <Hemal@DESKTOP-UN8MBH0>
2024-01-05 00:08:11 +0530

    Home

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -31,11 +31,11 @@ import { Link } from 'react-router-dom';
               id='navbarSupportedContent'
             >
               <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-                <li className='nav-item'>
+                {/* <li className='nav-item'>
                   <Link className='nav-link active' aria-current='page' to='/'>
                     Home
                   </Link>
-                </li>
+                </li> */}
                 
                 <li className='nav-item'>
                   <Link className='nav-link' to='/business'>
